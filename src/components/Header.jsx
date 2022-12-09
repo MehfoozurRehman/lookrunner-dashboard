f1283f14fcbeacfee1d6696405ffaf67967f73bd
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-12-09 20:36:50 +0500

    sidebar changings done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,9 @@
+import ThemeSwitch from "react-theme-switch-css";
+
 export default function Header() {
-  return <div className="container__main__header">Header</div>;
+  return (
+    <div className="container__main__header">
+      <ThemeSwitch />
+    </div>
+  );
 }
