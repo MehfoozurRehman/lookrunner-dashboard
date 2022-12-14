6dea99bfdb5581d02228fe10850abc3172f65576
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-12-14 19:50:10 +0500

    input, textarea and select done

--- src/screens/dashboard/employees/add.lazy.jsx
+++ src/screens/dashboard/employees/add.lazy.jsx
@@ -1,3 +1,328 @@
+import { Eye, EyeOff } from "react-feather";
+
+import { Link } from "router";
+import ReactSelect from "react-select";
+import { useBackLocation } from "global";
+import { useState } from "react";
+
 export default function EmployeeAdd() {
-  return <div>EmployeeAdd</div>;
+  const backLocation = useBackLocation();
+  return (
+    <div className="container__main__content__details">
+      <div className="container__main__content__details__main container__main__content__details__main__special">
+        <div className="container__main__content__details__main__row">
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+          <div className="container__main__content__details__main__row__entry">
+            <Select
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+              error="sdfkjsdkjflskajdflkjsdf"
+              options={[
+                { value: "1", label: "1" },
+                { value: "2", label: "2" },
+                { value: "3", label: "3" },
+              ]}
+            />
+          </div>
+        </div>
+        <div className="container__main__content__details__main__row">
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+          <div className="container__main__content__details__main__row__entry">
+            <Select
+              label="Name"
+              isMulti
+              placeholder="Enter your name"
+              id="name"
+              error="sdfkjsdkjflskajdflkjsdf"
+              options={[
+                { value: "1", label: "1" },
+                { value: "2", label: "2" },
+                { value: "3", label: "3" },
+                { value: "4", label: "4" },
+                { value: "5", label: "5" },
+                { value: "6", label: "6" },
+                { value: "7", label: "7" },
+                { value: "8", label: "8" },
+                { value: "9", label: "9" },
+                { value: "10", label: "10" },
+                { value: "11", label: "11" },
+                { value: "12", label: "12" },
+                { value: "13", label: "13" },
+                { value: "14", label: "14" },
+                { value: "15", label: "15" },
+                { value: "16", label: "16" },
+                { value: "17", label: "17" },
+              ]}
+            />
+          </div>
+        </div>
+        <div className="container__main__content__details__main__row">
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+        </div>
+        <div className="container__main__content__details__main__row">
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+        </div>
+        <div className="container__main__content__details__main__row">
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+        </div>
+        <div className="container__main__content__details__main__row">
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+        </div>
+        <div className="container__main__content__details__main__row">
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+        </div>
+        <div className="container__main__content__details__main__row">
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+        </div>
+        <Input
+          type="text"
+          label="Name"
+          placeholder="Enter your name"
+          id="name"
+        />
+        <Input
+          type="password"
+          label="Name"
+          placeholder="Enter your name"
+          id="name"
+          secure={true}
+        />
+        <Select
+          label="Name"
+          placeholder="Enter your name"
+          id="name"
+          error="sdfkjsdkjflskajdflkjsdf"
+          options={[
+            { value: "1", label: "1" },
+            { value: "2", label: "2" },
+            { value: "3", label: "3" },
+          ]}
+        />
+        <Textarea label="Name" placeholder="Enter your name" id="name" />
+      </div>
+      <div className="container__main__content__details__buttons">
+        <Link
+          to={backLocation}
+          className="container__main__content__details__buttons__button container__main__content__details__buttons__primary"
+        >
+          Add
+        </Link>
+        <Link
+          to={backLocation}
+          className="container__main__content__details__buttons__button container__main__content__details__buttons__secondary"
+        >
+          Delete
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+function Select({ label, id, error, ...props }) {
+  return (
+    <div className="container__main__content__details__main__input">
+      <label
+        htmlFor={id}
+        className="container__main__content__details__main__input__label"
+      >
+        {label}
+      </label>
+      <div
+        className="container__main__content__details__main__input__field__wrapper"
+        style={{ height: "fit-content" }}
+      >
+        <ReactSelect
+          {...props}
+          theme={(theme) => ({
+            ...theme,
+            borderRadius: 0,
+            colors: {
+              ...theme.colors,
+              primary75: "#2a5e59",
+              primary25: "#2a5e595e",
+              primary50: "#2a5e595e",
+              primary: "#2a5e59",
+            },
+          })}
+        />
+      </div>
+      {error && (
+        <div className="container__main__content__details__main__input__error">
+          {error}
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Input({ label, id, error, type, secure, ...props }) {
+  const [isSecure, setIsSecure] = useState(true);
+  return (
+    <div className="container__main__content__details__main__input">
+      <label
+        htmlFor={id}
+        className="container__main__content__details__main__input__label"
+      >
+        {label}
+      </label>
+      <div className="container__main__content__details__main__input__field__wrapper">
+        <input
+          type={isSecure ? type : "text"}
+          id={id}
+          name={id}
+          className="container__main__content__details__main__input__field"
+          {...props}
+        />
+        {secure && (
+          <button
+            className="container__main__content__details__main__input__button"
+            onClick={() => {
+              setIsSecure(!isSecure);
+            }}
+          >
+            {isSecure ? (
+              <EyeOff size={20} color="currentColor" />
+            ) : (
+              <Eye size={20} color="currentColor" />
+            )}
+          </button>
+        )}
+      </div>
+      {error && (
+        <div className="container__main__content__details__main__input__error">
+          {error}
+        </div>
+      )}
+    </div>
+  );
+}
+function Textarea({ label, id, error, type, ...props }) {
+  return (
+    <div className="container__main__content__details__main__input">
+      <label
+        htmlFor={id}
+        className="container__main__content__details__main__input__label"
+      >
+        {label}
+      </label>
+      <div className="container__main__content__details__main__input__field__wrapper container__main__content__details__main__input__field__wrapper__textarea">
+        <textarea
+          id={id}
+          name={id}
+          className="container__main__content__details__main__input__field"
+          {...props}
+        />
+      </div>
+      {error && (
+        <div className="container__main__content__details__main__input__error">
+          {error}
+        </div>
+      )}
+    </div>
+  );
 }
