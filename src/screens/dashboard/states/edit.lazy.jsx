6e241c0f9c0ee78d93e377922bd484cc00aa5ffd
M Abrar Khalid <abrarprince471@gmail.com>
2022-12-15 11:22:08 +0500

    States edit screen

--- src/screens/dashboard/states/edit.lazy.jsx
+++ src/screens/dashboard/states/edit.lazy.jsx
@@ -1,5 +1,43 @@
-import React from "react";
-
+import { Input, Select, Textarea, RichTextarea } from "../../../components";
+import { useBackLocation } from "global";
+import { Link } from "router";
 export default function statesEdit() {
-  return <div>statesEdit</div>;
+  const backLocation = useBackLocation();
+  return (
+    <div className="container__main__content__details">
+      <div className="container__main__content__details__main container__main__content__details__main__special">
+        <div className="container__main__content__details__main__row">
+          <div className="container__main__content__details__main__row__entry">
+            <Input type="text" label="Name" placeholder="Name" id="Name" />
+          </div>
+          <div className="container__main__content__details__main__row__entry">
+            <Select
+              label="Status"
+              placeholder="Active"
+              id="Status"
+              error="Select State"
+              options={[
+                { value: "Active", label: "Active" },
+                { value: "Offline", label: "offline" },
+              ]}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="container__main__content__details__buttons">
+        <Link
+          to={backLocation}
+          className="container__main__content__details__buttons__button container__main__content__details__buttons__primary"
+        >
+          Save
+        </Link>
+        <Link
+          to={backLocation}
+          className="container__main__content__details__buttons__button container__main__content__details__buttons__secondary"
+        >
+          Discard
+        </Link>
+      </div>
+    </div>
+  );
 }
