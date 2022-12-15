8deb992fbf79a40de36258b1b6422e80f1a6771f
Asim Rasheed <asimwarraich533@gmail.com>
2022-12-15 07:27:29 +0500

    roles screens done
     slug/edit/add screen done

--- src/screens/dashboard/roles/add.lazy.jsx
+++ src/screens/dashboard/roles/add.lazy.jsx
@@ -1,3 +1,49 @@
+import { Input, Select } from "../../../components";
+
+import { Link } from "router";
+import { useBackLocation } from "global";
+
 export default function RoleAdd() {
-  return <div>RoleAdd</div>;
+  const backLocation = useBackLocation();
+  return (
+    <div className="container__main__content__details">
+      <div className="container__main__content__details__main container__main__content__details__main__special">
+        <div className="container__main__content__details__main__row">
+          <div className="container__main__content__details__main__row__entry">
+            <Input
+              type="text"
+              label="Name"
+              placeholder="Enter your name"
+              id="name"
+            />
+          </div>
+        </div>
+        <Select
+          label="Select modules"
+          placeholder="Select modules"
+          id="Select modules"
+          error="Select modules"
+          options={[
+            { value: "User management", label: "User management" },
+            { value: "Order management", label: "Order management" },
+            { value: "management", label: "management" },
+          ]}
+        />
+      </div>
+      <div className="container__main__content__details__buttons">
+        <Link
+          to={backLocation}
+          className="container__main__content__details__buttons__button container__main__content__details__buttons__primary"
+        >
+          Save
+        </Link>
+        <Link
+          to={backLocation}
+          className="container__main__content__details__buttons__button container__main__content__details__buttons__secondary"
+        >
+          Discard
+        </Link>
+      </div>
+    </div>
+  );
 }
