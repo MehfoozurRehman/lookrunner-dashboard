7e0124c20a64a0a6c3e632cd36bdc2686d2d4015
M Abrar Khalid <abrarprince471@gmail.com>
2022-12-15 10:22:55 +0500

    SupplierDetails screen

--- src/screens/dashboard/Suppliers/[slug].lazy.jsx
+++ src/screens/dashboard/Suppliers/[slug].lazy.jsx
@@ -1,3 +1,39 @@
+import { DetailsEntry } from "../../../components";
+import { Link } from "router";
+import { useBackLocation } from "global";
+
 export default function SupplierDetails() {
-  return <div>SupplierDetails</div>;
+  const backLocation = useBackLocation();
+  return (
+    <div className="container__main__content__details">
+      <div className="container__main__content__details__main">
+        <DetailsEntry label="Company">Hello Pakistsan</DetailsEntry>
+        <DetailsEntry label="Brand name">Stylo</DetailsEntry>
+        <DetailsEntry label="Vat number">+12345678</DetailsEntry>
+        <DetailsEntry label="Email Address">
+          iabrarkhalidofficial@gmail.com
+        </DetailsEntry>
+        <DetailsEntry label="Mobile no.">+93 546 45667654</DetailsEntry>
+        <DetailsEntry label="Address"></DetailsEntry>
+        <DetailsEntry label="City">Lahore</DetailsEntry>
+        <DetailsEntry label="State">Punjab</DetailsEntry>
+        <DetailsEntry label="Postal code">37000</DetailsEntry>
+        <DetailsEntry label="Company description"></DetailsEntry>
+      </div>
+      <div className="container__main__content__details__buttons">
+        <Link
+          to={backLocation + "/edit"}
+          className="container__main__content__details__buttons__button container__main__content__details__buttons__primary"
+        >
+          Edit
+        </Link>
+        <Link
+          to={backLocation}
+          className="container__main__content__details__buttons__button container__main__content__details__buttons__secondary"
+        >
+          Delete
+        </Link>
+      </div>
+    </div>
+  );
 }
