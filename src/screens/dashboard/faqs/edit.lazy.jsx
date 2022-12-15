14ef8586eb58de8bf8b8fc15aef1f521bb771682
M Abrar Khalid <abrarprince471@gmail.com>
2022-12-15 17:35:13 +0500

    Remove  faqEdit replace asim code

--- src/screens/dashboard/faqs/edit.lazy.jsx
+++ src/screens/dashboard/faqs/edit.lazy.jsx
@@ -1,7 +1,7 @@
-import { Input, Select, Textarea, RichTextarea } from "../../../components";
+import { Input, Select, Textarea } from "../../../components";
 import { useBackLocation } from "global";
 import { Link } from "router";
-export default function faqEdit() {
+export default function stateAdd() {
   const backLocation = useBackLocation();
   return (
     <div className="container__main__content__details">
@@ -12,14 +12,14 @@ export default function faqEdit() {
           placeholder="Enter your Question"
           id="Question"
         />
-
         <Textarea
-          label="Answer"
-          placeholder="Enter your Answer..."
-          id="naAnswerme"
-          error="Enter your Answer"
+          label="Description"
+          placeholder="Enter your Description"
+          id="Description"
+          error="Enter your Description"
         />
       </div>
+
       <div className="container__main__content__details__buttons">
         <Link
           to={backLocation}
