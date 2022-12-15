b3d6a3126aef39b7d259a63602041f771fff799f
Asim Rasheed <asimwarraich533@gmail.com>
2022-12-15 10:26:34 +0500

    categories screen done (missing img input)

--- src/screens/dashboard/Categories/edit.lazy.jsx
+++ src/screens/dashboard/Categories/edit.lazy.jsx
@@ -1,5 +1,58 @@
-import React from "react";
+import { Input, Select, Textarea } from "../../../components";
+import { useBackLocation } from "global";
+import { Link } from "router";
+export default function supplierAdd() {
+  const backLocation = useBackLocation();
+  return (
+    <div className="container__main__content__details">
+      <div className="container__main__content__details__main container__main__content__details__main__special">
+        <Input
+          type="text"
+          label="Name"
+          placeholder="Enter your name"
+          id="Name"
+        />
 
-export default function CategoriesAdit() {
-  return <div>CategoriesAdit</div>;
+        <Textarea
+          label="Description"
+          placeholder="Enter your Description"
+          id="Description"
+          error="Enter your Description"
+        />
+        <Select
+          label="Parent Category"
+          isMulti
+          placeholder="Enter Parent Category"
+          id="name"
+          error="Select Parent Category"
+          options={[
+            { value: "1", label: "1" },
+            { value: "2", label: "2" },
+            { value: "3", label: "3" },
+            { value: "4", label: "4" },
+            { value: "5", label: "5" },
+            { value: "6", label: "6" },
+            { value: "7", label: "7" },
+            { value: "8", label: "8" },
+            { value: "9", label: "9" },
+          ]}
+        />
+      </div>
+
+      <div className="container__main__content__details__buttons">
+        <Link
+          to={backLocation}
+          className="container__main__content__details__buttons__button container__main__content__details__buttons__primary"
+        >
+          Save
+        </Link>
+        <Link
+          to={backLocation}
+          className="container__main__content__details__buttons__button container__main__content__details__buttons__secondary"
+        >
+          Discard
+        </Link>
+      </div>
+    </div>
+  );
 }
