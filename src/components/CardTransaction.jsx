a0e04973f0e3e1c3287bbcdcbdac527ed8a16d28
Asim Rasheed <asimwarraich533@gmail.com>
2022-12-20 07:57:54 +0500

    order screen timeline and transaction card done

--- src/components/CardTransaction.jsx
+++ src/components/CardTransaction.jsx
@@ -13,14 +13,30 @@ export default function CardTransaction({}) {
         </div>
       </div>
       <div className="order__details__main__customer__card__content">
-        <div className="order__details__main__customer__card__content__row">
-          <div className="order__details__main__customer__card__content__row__label">
-            Authorized
-          </div>
-          <div className="order__details__main__customer__card__content__row__value">
-            Aur-163
-          </div>
-        </div>
+        <CardTransactionRow text="Authorized" value="Aur-163" />
+        <CardTransactionRow text="Created" value="14/13/2022" />
+        <CardTransactionRow text="UID" value="23243443543434" />
+        <CardTransactionRow text="Action ID" value="1212121234" />
+        <CardTransactionRow text="Status" value="Approved" />
+        <CardTransactionRow text="Card" value="XXXXX XXXX-----" />
+        <CardTransactionRow text="Reference" value="Auth-che-4335463" />
+        <CardTransactionRow text="Auth Code" value="3435465756" />
+        <CardTransactionRow
+          text="Transaction for order ids"
+          value="3435465756"
+        />
+      </div>
+    </div>
+  );
+}
+function CardTransactionRow({ text, value }) {
+  return (
+    <div className="order__details__main__customer__card__content__row__card__transaction">
+      <div className="order__details__main__customer__card__content__row__label">
+        {text}
+      </div>
+      <div className="order__details__main__customer__card__content__row__value">
+        {value}
       </div>
     </div>
   );
--- src/components/TimelineCard.jsx
+++ src/components/TimelineCard.jsx
@@ -4,7 +4,67 @@ export default function TimelineCard({}) {
       <div className="order__details__main__customer__card__header">
         Timeline
       </div>
-      <div className="order__details__main__customer__card__content"></div>
+      <div className="order__details__main__customer__card__content__timeline__upper__wraper">
+        <div className="order__details__main__customer__card__content__timeline__heading__container">
+          <div className="order__details__main__customer__card__content__timeline__upper__heading">
+            1 SEPTEMBER
+          </div>
+          <div className="order__details__main__customer__card__content__timeline__upper__sub__heading">
+            You archived this order.
+          </div>
+        </div>
+        <div className="order__details__main__customer__card__content__timeline__time">
+          5:06pm
+        </div>
+      </div>
+      <div className="order__details__main__customer__card__content__timeline__upper__wraper">
+        <div className="order__details__main__customer__card__content__timeline__heading__container">
+          <div className="order__details__main__customer__card__content__timeline__upper__heading">
+            18 AUGUST
+          </div>
+          <div className="order__details__main__customer__card__content__timeline__upper__sub__heading">
+            Customer selected return date 18/12/2022.
+          </div>
+          <div className="order__details__main__customer__card__content__timeline__upper__sub__heading">
+            Customers taking 3 articles.
+          </div>
+          <div className="order__details__main__customer__card__content__timeline__upper__sub__heading">
+            Return initiated by the Customer.
+          </div>
+          <div className="order__details__main__customer__card__content__timeline__upper__sub__heading">
+            Booking confirmation message was sent to Rashid Muayyed
+            (+923219652222)
+          </div>
+        </div>
+        <div className="order__details__main__customer__card__content__timeline__time">
+          5:06pm
+        </div>
+      </div>
+      <TimelineDetail heading="$6 paid for delivery charges" time="5:40pm" />
+      <TimelineDetail
+        heading="Confirmation #Zt08KYTU7 was generated for this order.
+"
+        time="5:40pm"
+      />
+      <TimelineDetail
+        heading="Rashid Muayyed booked this order.
+"
+        time="5:40pm"
+      />
+    </div>
+  );
+}
+
+function TimelineDetail({ heading, time }) {
+  return (
+    <div className="order__details__main__customer__card__content__timeline">
+      <div className="order__details__main__customer__card__content__timeline__heading">
+        {heading}
+      </div>
+
+      <div className="order__details__main__customer__card__content__timeline__time">
+        {time}
+      </div>
     </div>
   );
 }
