ced0c315dc9c5eef0ac2c2fb1a9d0356a8440821
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-12-22 20:35:58 +0500

    order details done

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 
-export default function Step({ active }) {
+export default function Step({ active, label }) {
   return (
     <div className="order__details__steps__content__step">
       <div
@@ -15,7 +15,7 @@ export default function Step({ active }) {
           active && "order__details__steps__content__step__line__active"
         )}
       />
-      <div className="order__details__steps__content__step__label">Ordered</div>
+      <div className="order__details__steps__content__step__label">{label}</div>
       <div className="order__details__steps__content__step__details">
         <span>Date</span>
         22/02/2021
--- src/screens/dashboard/orders/[slug].lazy.jsx
+++ src/screens/dashboard/orders/[slug].lazy.jsx
@@ -7,24 +7,12 @@ import ScrollContainer from "react-indiana-drag-scroll";
 import SendMessageCard from "components/SendMessageCard";
 import Step from "components/Step";
 import TimelineCard from "components/TimelineCard";
+import { useState } from "react";
 
 export default function OrderDetails() {
   return (
     <div className="order__details">
-      <div className="order__details__steps">
-        <div className="order__details__steps__heading">Order Details</div>
-        <ScrollContainer className="order__details__steps__content">
-          <Step active={true} />
-          <Step active={true} />
-          <Step active={true} />
-          <Step />
-          <Step />
-          <Step />
-        </ScrollContainer>
-        <button className="order__details__steps__button">
-          Move to delivered
-        </button>
-      </div>
+      <OrderStatus />
       <div className="order__details__main">
         <div className="order__details__main__row">
           <OrderDetailsCard
@@ -53,6 +41,42 @@ export default function OrderDetails() {
           </div>
         </div>
       </div>
+    </div>
+  );
+}
+
+function OrderStatus() {
+  const [activeStep, setActiveStep] = useState(0);
+  return (
+    <div className="order__details__steps">
+      <div className="order__details__steps__heading">Order Details</div>
+      <ScrollContainer className="order__details__steps__content">
+        <Step label="Ordered" active={activeStep >= 0} />
+        <Step label="Delivered" active={activeStep >= 1} />
+        <Step label="Waiting for Pickup" active={activeStep >= 2} />
+        <Step label="Return Initiated" active={activeStep >= 3} />
+        <Step label="Picked up" active={activeStep >= 4} />
+        <Step label="Payment Completed" active={activeStep >= 5} />
+      </ScrollContainer>
+      <button
+        className="order__details__steps__button"
+        disabled={activeStep === 5}
+        onClick={() => {
+          setActiveStep(activeStep + 1);
+        }}
+      >
+        {activeStep === 0
+          ? "Move to delivered"
+          : activeStep === 1
+          ? "Move to waiting for pickup"
+          : activeStep === 2
+          ? "Move to return initiated"
+          : activeStep === 3
+          ? "Move to picked up"
+          : activeStep === 4
+          ? "Move to payment Completed"
+          : "Finished"}
+      </button>
     </div>
   );
 }
