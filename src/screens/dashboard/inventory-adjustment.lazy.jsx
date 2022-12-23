38da5036b8c433d6cebec23b2024e7a2b76c7076
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-12-24 02:27:14 +0500

    inventory adjustment panel done

--- src/screens/dashboard/inventory-adjustment.lazy.jsx
+++ src/screens/dashboard/inventory-adjustment.lazy.jsx
@@ -2,6 +2,8 @@ import { Avatar, TableEntryText } from "components";
 
 import { Search } from "react-feather";
 import Select from "react-select";
+import { useState } from "react";
+import ClickAwayListener from "react-click-away-listener";
 
 export default function Inventoryadjustment() {
   return (
@@ -84,6 +86,7 @@ export default function Inventoryadjustment() {
 }
 
 function TableEntry() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="container__main__content__listing__table__content__list">
       <TableEntryText style={{ width: "200px" }}>
@@ -118,12 +121,53 @@ function TableEntry() {
       <TableEntryText>Crosby Green Crochet Sweater - Small</TableEntryText>
       <TableEntryText>in Stock</TableEntryText>
       <TableEntryText>Lahore</TableEntryText>
-      <TableEntryText>
-        <input type="number" placeholder="0" />
-        <button className="container__main__content__listing__table__content__list__entry__special__button">
-          Save
-        </button>
-      </TableEntryText>
+      <ClickAwayListener
+        onClickAway={() => {
+          setOpen(false);
+        }}
+      >
+        <TableEntryText>
+          {open && (
+            <div className="container__main__content__listing__table__content__list__entry__special__panel">
+              <label htmlFor="">Adjust by</label>
+              <input
+                type="number"
+                placeholder="0"
+                className="container__main__content__listing__table__content__list__entry__special__panel__input"
+              />
+              <label htmlFor="">Reason</label>
+              <select
+                name=""
+                id=""
+                className="container__main__content__listing__table__content__list__entry__special__panel__input"
+              >
+                <option value="">Correction</option>
+                <option value="">Count</option>
+                <option value="">Recieved</option>
+                <option value="">Return restock</option>
+                <option value="">Damaged</option>
+                <option value="">Theft or loss</option>
+                <option value="">Promotion or donation</option>
+              </select>
+            </div>
+          )}
+          <input
+            type="number"
+            placeholder="0"
+            onFocus={() => {
+              setOpen(true);
+            }}
+          />
+          <button
+            className="container__main__content__listing__table__content__list__entry__special__button"
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            Save
+          </button>
+        </TableEntryText>
+      </ClickAwayListener>
     </div>
   );
 }
