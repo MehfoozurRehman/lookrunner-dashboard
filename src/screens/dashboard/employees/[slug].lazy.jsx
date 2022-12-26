26be23657ebbd7ad437b783a9f8bf28dbb17c735
M Abrar Khalid <abrarprince471@gmail.com>
2022-12-26 19:43:58 +0500

    description is not in design but i think it's okay to be there. Am i right?

--- src/screens/dashboard/employees/[slug].lazy.jsx
+++ src/screens/dashboard/employees/[slug].lazy.jsx
@@ -18,20 +18,6 @@ export default function EmployeeDetails() {
         <DetailsEntry label="Password" secure={true}>
           23904567
         </DetailsEntry>
-        <DetailsEntry label="Description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-          reprehenderit expedita et tenetur nulla saepe officia iure alias
-          possimus, beatae consequatur exercitationem error dignissimos.
-          Tempore, quo fuga quae earum hic illum saepe unde? Natus ducimus,
-          voluptate totam laudantium, quam ullam veniam officiis placeat vitae
-          eaque hic ut fuga error ipsum deleniti tempore quia cumque earum!
-          Exercitationem quam possimus, quod nam explicabo magni nesciunt
-          deleniti, impedit labore enim eum necessitatibus, beatae corporis?
-          Dolor repellendus veniam, praesentium harum quaerat necessitatibus
-          atque, cupiditate iure veritatis perspiciatis ipsum est velit numquam
-          dolorum alias ipsam repudiandae in nisi! Nam alias libero est error
-          perferendis dolorum.
-        </DetailsEntry>
       </div>
       <div className="container__main__content__details__buttons">
         <Link
